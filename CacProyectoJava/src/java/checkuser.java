d60af4e008bdadcd180d2108e99cae082b603888
Jeferson Borges <jefersonjbsousa@gmail.com>
2022-07-17 18:55:14 -0300

    Add files via upload

---
+++ CacProyectoJava/src/java/checkuser.java
@@ -0,0 +1,105 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Jeferson
+ */
+@WebServlet(urlPatterns = {"/checkuser"})
+public class checkuser extends HttpServlet {
+
+    private static String classgetName() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     * @throws java.sql.SQLException
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            Persistencia base= new Persistencia();
+            ResultSet rs = base.consultaSQL("select * from usuarios where usuario ="
+            + "'" + request.getParameter("inputEmail") + "'" + " and clave = " + "'" +
+                    request.getParameter("inputPassword") + "'");
+                    
+                
+                while(rs.next()){
+                   out.println(rs.getString("usuario"));
+                   //out.println(rs.getString("clave"));
+                   out.println(rs.getString("nombreyapellido")+"<BR>");
+                }
+                
+                if(rs.first()==false){
+                out.println("No hay usuarios que coincidan con la búsqueda");
+                out.println("<h1>Proyecto: " + request.getContextPath() + "</h1>");
+                out.println("<h1>Usuario  : " + request.getParameter("inputEmail") + "</h1>");
+            }
+         
+        } catch (SQLException ex)
+            Logger.getLogger(checkuser.classgetName()).log(Level.SEVERE, null, ex);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
+}
